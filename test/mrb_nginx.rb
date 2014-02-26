502483f93ad94a5523d4b991f62e841b8dc74a01
MATSUMOTO Ryosuke <matsumoto_r@net.ist.i.kyoto-u.ac.jp>
2014-02-26 18:17:40 +0900

    Remove default test

--- test/mrb_nginx.rb
+++ test/mrb_nginx.rb
@@ -2,16 +2,3 @@
 ## Nginx Test
 ##
 
-assert("Nginx#hello") do
-  t = Nginx.new "hello"
-  assert_equal("hello", t.hello)
-end
-
-assert("Nginx#bye") do
-  t = Nginx.new "hello"
-  assert_equal("hello bye", t.bye)
-end
-
-assert("Nginx.hi") do
-  assert_equal("hi!!", Nginx.hi)
-end
