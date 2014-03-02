f576293b22f154d6ffbc8386ee1dbb1d5653f647
MATSUMOTO Ryosuke <matsumoto_r@net.ist.i.kyoto-u.ac.jp>
2014-03-02 11:37:29 +0900

    Add Apache::Request#document_root

--- mrblib/mrb_nginx.rb
+++ mrblib/mrb_nginx.rb
@@ -1,4 +1,12 @@
 class Nginx
+  class Request
+    def document_root
+      Nginx::Server.new.document_root
+    end
+    #def document_root=(path)
+    #  Nginx::Var.new.set "document_root", path
+    #end
+  end
   class Headers_in
     def user_agent
       self["User-Agent"]
